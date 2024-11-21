b98715767dee7b5b33e8ca0dc99f22115e42bfe7
minhdavidnguyen <132498525+minhdavidnguyen@users.noreply.github.com>
2024-11-20 20:08:39 -0700

    Resolve mismatch between database and java model
    Changed userId datatype from Long to Integer to match SQL script and configured Hibernate naming strategy so the model does not automatically change column names from camelCase to snake case (e.g., userId won't get changed to user_Id)

--- Backend/acmeplex/src/main/java/org/example/acmeplex/repository/RegisteredUserRepository.java
+++ Backend/acmeplex/src/main/java/org/example/acmeplex/repository/RegisteredUserRepository.java
@@ -3,7 +3,7 @@ package org.example.acmeplex.repository;
 import org.example.acmeplex.model.RegisteredUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RegisteredUserRepository extends JpaRepository<RegisteredUser, Long> {
+public interface RegisteredUserRepository extends JpaRepository<RegisteredUser, Integer> {
     RegisteredUser findByUsername(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);

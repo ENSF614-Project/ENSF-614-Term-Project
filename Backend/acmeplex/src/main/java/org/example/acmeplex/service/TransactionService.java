97f6bb917b7bba09247dde3f7d1a2c6f66ffc600
Ahmed Shehata <110998057+AhmedShehata99@users.noreply.github.com>
2024-11-26 07:46:31 -0700

    Update TransactionService.java

--- Backend/acmeplex/src/main/java/org/example/acmeplex/service/TransactionService.java
+++ Backend/acmeplex/src/main/java/org/example/acmeplex/service/TransactionService.java
@@ -4,7 +4,6 @@ import org.example.acmeplex.model.Transaction;
 import org.example.acmeplex.repository.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
 
 @Service
 public class TransactionService {
@@ -13,12 +12,7 @@ public class TransactionService {
     private TransactionRepository transactionRepository;
 
     public Transaction createTransaction(Transaction transaction) {
-        transaction.setTransactionDate(new java.util.Date());
         return transactionRepository.save(transaction);
-    }
-
-    public List<Transaction> getTransactionsByUser(Integer userID) {
-        return transactionRepository.findByUserID(userID);
     }
 
     public Transaction getTransactionById(Integer transactionID) {

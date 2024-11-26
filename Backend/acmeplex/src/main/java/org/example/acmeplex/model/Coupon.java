40f2e7f846abcea79a43c7bb3b2163dbc62f7152
Ahmed Shehata <110998057+AhmedShehata99@users.noreply.github.com>
2024-11-26 08:20:49 -0700

    Update Coupon.java

--- Backend/acmeplex/src/main/java/org/example/acmeplex/model/Coupon.java
+++ Backend/acmeplex/src/main/java/org/example/acmeplex/model/Coupon.java
@@ -14,33 +14,17 @@ public class Coupon {
     @Column(name = "couponID", nullable = false)
     private Integer couponID;
 
-    @Column(name = "ticketID", nullable = false)
-    private Integer ticketID;
-
-    @Column(name = "email", nullable = false)
-    private String email;
-
-    @Column(name = "userID", nullable = false)
-    private Integer userID;
+    @Column(name = "value", nullable = false)
+    private Double value;
 
     @Column(name = "expiryDate", nullable = false)
     @Temporal(TemporalType.DATE)
     private Date expiryDate;
 
-    @Column(name = "value", nullable = false)
-    private Double value;
-
-    @Column(name = "amount", nullable = false)
-    private Double amount;
-
     @Column(name = "status", nullable = false, length = 50)
     private String status;
 
     @ManyToOne
-    @JoinColumn(name = "ticketID", insertable = false, updatable = false)
-    private Ticket ticket;
-
-    @ManyToOne
-    @JoinColumn(name = "email", referencedColumnName = "email", insertable = false, updatable = false)
+    @JoinColumn(name = "userId")
     private User user;
 }

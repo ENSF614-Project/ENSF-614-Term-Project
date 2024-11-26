76c264ee76e6cbb8f0be89e095b05f5e3aa13bdf
Ahmed Shehata <110998057+AhmedShehata99@users.noreply.github.com>
2024-11-26 06:02:54 -0700

    Delete Backend/acmeplex/src/main/java/org/example/acmeplex/repository/TicketRepository.java

--- Backend/acmeplex/src/main/java/org/example/acmeplex/repository/TicketRepository.java
+++
@@ -1,6 +0,0 @@
-package org.example.acmeplex.repository;
-
-import org.example.acmeplex.model.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Integer> {}

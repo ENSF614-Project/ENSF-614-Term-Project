a747977fb7a53d4efb76095912d6edf5076dbea0
Ahmed Shehata <110998057+AhmedShehata99@users.noreply.github.com>
2024-11-26 09:55:31 -0700

    Update TicketRepository.java

--- Backend/acmeplex/src/main/java/org/example/acmeplex/repository/TicketRepository.java
+++ Backend/acmeplex/src/main/java/org/example/acmeplex/repository/TicketRepository.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface TicketRepository extends JpaRepository<Ticket, Integer> {
     List<Ticket> findByShowtimeShowtimeIdAndStatus(Integer showtimeId, String status);
     List<Ticket> findByUserUserId(Integer userId);
+    List<Ticket> findByTransactionTransactionID(Integer transactionID);
 }

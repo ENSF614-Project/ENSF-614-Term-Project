aa32fa9ba8ce752693cf8b281c0f533cafe57975
Ahmed Shehata <110998057+AhmedShehata99@users.noreply.github.com>
2024-11-26 07:43:47 -0700

    Update CouponRepository.java

--- Backend/acmeplex/src/main/java/org/example/acmeplex/repository/CouponRepository.java
+++ Backend/acmeplex/src/main/java/org/example/acmeplex/repository/CouponRepository.java
@@ -2,11 +2,5 @@ package org.example.acmeplex.repository;
 
 import org.example.acmeplex.model.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
 
-@Repository
-public interface CouponRepository extends JpaRepository<Coupon, Integer> {
-    List<Coupon> findByUserID(Integer userID);
-    Coupon findByTicketID(Integer ticketID);
-}
+public interface CouponRepository extends JpaRepository<Coupon, Integer> {}

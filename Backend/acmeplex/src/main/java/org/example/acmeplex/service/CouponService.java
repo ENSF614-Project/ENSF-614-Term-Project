bab3d7b35cf509cb3caa3b50399b2e99af16b065
Ahmed Shehata <110998057+AhmedShehata99@users.noreply.github.com>
2024-11-26 08:23:07 -0700

    Update CouponService.java

--- Backend/acmeplex/src/main/java/org/example/acmeplex/service/CouponService.java
+++ Backend/acmeplex/src/main/java/org/example/acmeplex/service/CouponService.java
@@ -2,20 +2,20 @@ package org.example.acmeplex.service;
 
 import org.example.acmeplex.model.Coupon;
 import org.example.acmeplex.repository.CouponRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CouponService {
 
-    @Autowired
-    private CouponRepository couponRepository;
+    private final CouponRepository couponRepository;
 
-    public Coupon createCoupon(Coupon coupon) {
-        return couponRepository.save(coupon);
+    public CouponService(CouponRepository couponRepository) {
+        this.couponRepository = couponRepository;
     }
 
-    public boolean validateCoupon(Coupon coupon) {
-        return coupon.getExpiryDate().after(new Date()) && coupon.getStatus().equals("Valid");
+    public List<Coupon> getValidCouponsByUserId(Integer userId) {
+        return couponRepository.findByUserUserIdAndExpiryDateAfterAndStatus(userId, new Date(), "Valid");
     }
 }

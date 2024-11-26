aa9bb44558045762ee5ebf46ecda1f691d1d9f5e
Ahmed Shehata <110998057+AhmedShehata99@users.noreply.github.com>
2024-11-26 07:44:18 -0700

    Update TransactionRepository.java

--- Backend/acmeplex/src/main/java/org/example/acmeplex/repository/TransactionRepository.java
+++ Backend/acmeplex/src/main/java/org/example/acmeplex/repository/TransactionRepository.java
@@ -2,10 +2,5 @@ package org.example.acmeplex.repository;
 
 import org.example.acmeplex.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.List;
 
-@Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-    List<Transaction> findByUserID(Integer userID);
-}
+public interface TransactionRepository extends JpaRepository<Transaction, Integer> {}

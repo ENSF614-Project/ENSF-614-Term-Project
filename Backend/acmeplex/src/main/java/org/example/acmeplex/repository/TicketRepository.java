b7c93342317dc428f3fad1da58b5584b1622ddbe
Ahmed Shehata <110998057+AhmedShehata99@users.noreply.github.com>
2024-11-27 01:42:19 -0700

    Update TicketRepository.java

--- Backend/acmeplex/src/main/java/org/example/acmeplex/repository/TicketRepository.java
+++ Backend/acmeplex/src/main/java/org/example/acmeplex/repository/TicketRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     List<Ticket> findByUser(User user);
-    List<Ticket> findByShowtime(Showtime showtime);
+    List<Ticket> findByShowtimeID(Integer showtime);
 }

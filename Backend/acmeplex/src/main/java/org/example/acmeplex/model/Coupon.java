3ba87ceca1aeb7ac0b92be1e830b95cc46fd97b6
Ahmed Shehata <110998057+AhmedShehata99@users.noreply.github.com>
2024-11-27 01:33:11 -0700

    Add files via upload

---
+++ Backend/acmeplex/src/main/java/org/example/acmeplex/model/Coupon.java
@@ -0,0 +1,36 @@
+package org.example.acmeplex.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "COUPON")
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long couponID;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+
+    @Column(name = "value", nullable = false)
+    private Double value;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "expiry_date", nullable = false)
+    private Date expiryDate;
+
+    @Column(nullable = false, length = 50)
+    private String status;
+}

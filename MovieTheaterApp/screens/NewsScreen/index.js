9d73baa921bf3fb7b3751aec353e278d0d9071e2
Riley-Martel <79805828+Riley-Martel@users.noreply.github.com>
2024-11-30 14:59:47 -0700

    Removed News MockData
    Removed the mock data in news and got rid of in page log in verification

--- MovieTheaterApp/screens/NewsScreen/index.js
+++ MovieTheaterApp/screens/NewsScreen/index.js
@@ -3,23 +3,11 @@ import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
 import { styles } from './styles';
 import { getEarlyAccessNotifications } from "../../services/api.js";
 
-const NewsScreen = ({ navigation }) => {
-    const [isUserLoggedIn, setIsUserLoggedIn] = useState(true); // Replace with actual login logic
+const NewsScreen = ({ }) => {
     const [notifications, setNotifications] = useState([]); // State for notifications
 
-    // Mock data
-    const [news] = useState([
-        { type: 'Release', news: 'Big movie coming out next week, preorder your tickets now for Spooderman 420!' },
-        { type: 'Concession', news: 'Acmeplex is proud to announce that it will now be serving lunchly at all our locations!' }
-    ]);
-
     useEffect(() => {
-        // Redirect to login if the user is not logged in
-        if (!isUserLoggedIn) {
-            navigation.navigate('Login');
-        }
 
-        // Fetch notifications
         getEarlyAccessNotifications()
             .then((data) => {
                 console.log("Fetched notifications:", data);
@@ -28,12 +16,12 @@ const NewsScreen = ({ navigation }) => {
             .catch((error) => {
                 console.error("Error fetching notifications:", error);
             });
-    }, [isUserLoggedIn, navigation]);
-
+    }, []);
+ 
     return (
         <View style={styles.container}>
             <Text style={styles.title}>News</Text>
-            {isUserLoggedIn && (
+            {(
                 <ScrollView style={styles.newsContainer}>
                     {/* Render Notifications */}
                     {notifications.length > 0 && (
@@ -44,14 +32,6 @@ const NewsScreen = ({ navigation }) => {
                             ))}
                         </View>
                     )}
-
-                    {/* Render Mock News */}
-                    {news.map((item, index) => (
-                        <View key={index} style={styles.newsItem}>
-                            <Text style={styles.newsType}>{item.type}</Text>
-                            <Text style={styles.newsText}>{item.news}</Text>
-                        </View>
-                    ))}
                 </ScrollView>
             )}
         </View>

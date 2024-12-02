8d0651102d5a91e179dd34c421a699c34aba7159
Riley-Martel <79805828+Riley-Martel@users.noreply.github.com>
2024-12-01 18:47:15 -0700

    Finished Account
    Finished adding account api

--- MovieTheaterApp/services/accountService.js
+++ MovieTheaterApp/services/accountService.js
@@ -1,15 +1,27 @@
 // services/accountService.js
 const API_URL = 'http://localhost:8080';
 
-export const registerService = {
+export const accountService = {
     async getUser(userId) {
         try {
             const response = await fetch(`${API_URL}/api/users/${userId}`);
-            if (!response.ok) throw new Error('Failed to fetch tickets');
+            if (!response.ok) throw new Error('Failed to get user');
             return await response.json();
         } catch (error) {
                 console.error('Error getting user by user ID:', error);
                 throw error;
             }
-        }
+        },
+
+
+        async getPaymentInfo(userId) {
+            try {
+                const response = await fetch(`${API_URL}/api/paymentinfo/user/${userId}`);
+            if (!response.ok) throw new Error('Failed to get paymentinfo');
+            return await response.json();
+        } catch (error) {
+                console.error('Error getting paymentinfo by user ID:', error);
+                throw error;
+            }
+        },
 };
